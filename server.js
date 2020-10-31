22a9c2e9fcadd7d6c82e300027c1504843e3bc8d
Josh Hills <josh.hills@guerrilla-games.com>
2020-10-31 19:26:19 +0100

    test

--- server.js
+++ server.js
@@ -20,127 +20,171 @@ const wss = new Server({ server });
 const questions = [
     {
         number: 1,
-        text: 'Which of the following is NOT a ghost from the original Pac-Man?',
+        text: 'There’s a spooky, scary skeleton inside all of us. But in Among Us, how many bones are there inside each (human) crewmate?',
         options: {
-            a: 'Winky',
-            b: 'Clyde',
-            c: 'Blinky',
-            d: 'Inky'
+            a: '270',
+            b: '206',
+            c: '20',
+            d: '1'
         }
     },
     {
         number: 2,
-        text: 'Which of the following Scrabble tiles has the highest value?',
+        text: 'In the video game Lemmings, you attempt to lead a group of tiny animals to safety that are simultaneously difficult to keep alive. In real life, while Lemmings do migrate in herds, they are perfectly normal animals. Which of these is not a myth about Lemmings?',
         options: {
-            a: 'K',
-            b: 'Q',
-            c: 'J',
-            d: 'X'
+            a: 'They commit mass suicide by falling off cliffs',
+            b: 'They are spontaneously generated in the sky and fall to the earth like rain',
+            c: 'They explode when they become sufficiently angry',
+            d: 'They charge when they see the colour red'
         }
     },
     {
         number: 3,
-        text: 'Which of the following is a genuine Pokémon type?',
+        text: 'While there is no end to the game Tetris, after level 29 the game begins to glitch out. What is this state referred to, due to the difficulty doubling between level 28 and 29?',
         options: {
-            a: 'Earth',
-            b: 'Dark',
-            c: 'Air',
-            d: 'Light'
+            a: 'Kill Screen',
+            b: 'End Zone',
+            c: 'Speedrun Gold',
+            d: 'Tetronimo'
         }
     },
     {
         number: 4,
-        text: 'Under what name did Mario debut in 1981\'s Donkey Kong?',
+        text: 'A 2014 game spawned a meme due to a poor-taste quick-time event whereby the player is asked to “press F to pay respects” at the grave of a fallen soldier. Which game?',
         options: {
-            a: 'Luigi',
-            b: 'Jumpman',
-            c: 'Plumberman',
-            d: 'Mario'
+            a: 'Call of Duty: Black Ops',
+            b: 'Call of Duty: Modern Resurgence',
+            c: 'Call of Duty: Advanced Warfare',
+            d: 'Medal of Honor: Warfighter'
         }
     },
     {
         number: 5,
-        text: 'Which Nintendo console was part of the sixth generation of video game consoles along with the Sony Playstation 2 and the Microsoft Xbox?',
+        text: 'When Naughty dog were localising ‘Crash Bandicoot 2: Cortex Strikes Back’ for the Japanese market, they replaced one of the twenty-six death animations in the game, where Crash would be crushed to death leaving only his severed head and shoes. What was the reason for this censorship?',
         options: {
-            a: 'Wii',
-            b: 'SNES',
-            c: 'Gamecube',
-            d: 'Nintendo 64'
+            a: 'The shoes were Air Jordans',
+            b: 'It reminded people of a serial killer on the loose',
+            c: 'Cultural etiquette forbade unattended shoes outdoors',
+            d: 'The death was deemed too brutal for the intended age rating'
         }
     },
     {
         number: 6,
-        text: 'What genre of video game is League of Legends?',
+        text: 'In the month after Dark Souls II’s release in 2014, how many players died on average every second?',
         options: {
-            a: 'MOBA',
-            b: 'MMOFPS',
-            c: 'MMORPG',
-            d: 'MMORTS'
+            a: '4',
+            b: '42',
+            c: '110',
+            d: '2,410'
         }
     },
     {
         number: 7,
-        text: 'Which of the following is NOT a game mode in Minecraft?',
+        text: 'In the Greek mythology that inspired the game Hades, there are six rivers visible in the underworld. Their names were meant to reflect the emotions associated with death. Which of these is not a river?',
         options: {
-            a: 'Hardcore',
-            b: 'Spectator',
-            c: 'Adventure',
-            d: 'Explorer'
+            a: 'Phobos (Fear)',
+            b: 'Lethe (Forgetfulness)',
+            c: 'Phlegethon (Fire)',
+            d: 'Styx (Hatred)'
         }
     },
     {
         number: 8,
-        text: 'Which game in the Final Fantasy franchise was the first to spawn a direct sequel?',
+        text: 'The 1980s dungeon crawling game Rogue was the first game to feature ‘permadeath’, which inspired a subgenre of “roguelikes”; should the player lose all their health from combat or other means, they must start afresh without loading from a saved state. Why was intentional design choice made?',
         options: {
-            a: 'FFVII',
-            b: 'FFX',
-            c: 'FFV',
-            d: 'FFXII'
+            a: 'To pad the runtime as publishers thought content was lacking',
+            b: 'The Amiga did not have enough memory to support large save files',
+            c: 'Playtesters were repeatedly reloading save files to obtain the best results',
+            d: 'The developers wanted to make the hardest game ever made'
         }
     },
     {
         number: 9,
-        text: 'Which of the following consoles was released by Sega?',
+        text: 'In the core series of Pokémon games, when all of the Pokémon on the your team faint, you’re greeted with something comparable to a game over screen with a message. In Japanese versions of the first generation games, the player is said to “lose hope”, before typically reappearing at a Pokémon centre. Which of these is not a game over message to appear in the English versions?',
         options: {
-            a: 'Gamecube',
-            b: 'Zodiac',
-            c: 'Dreamcast',
-            d: 'Jaguar'
+            a: '<player> whited out!',
+            b: 'You were overwhelmed by your defeat!',
+            c: '<player> blacked out!',
+            d: 'You faded away…'
         }
     },
     {
         number: 10,
-        text: 'Which of the following model building toys was developed in Denmark?',
+        text: 'Which of these video game characters could survive skinny-dipping?',
         options: {
-            a: 'K\'NEX',
-            b: 'Lego',
-            c: 'Fischertechnik',
-            d: 'Meccano'
+            a: 'Blue Pikmin',
+            b: 'Altair',
+            c: 'John Marston',
+            d: 'Tommy Vercetti'
         }
     },
     {
         number: 11,
-        text: 'Which is NOT a dungeon in The Legend of Zelda: Ocarina of Time?',
+        text: 'The void is the name given to the empty space below the overworld in Minecraft, barred by an impenetrable protective layer of ‘bedrock’ at the bottom. You can reach it by digging directly down, but how many layers of bedrock are there at the bottom of the world, separating you from this void?',
         options: {
-            a: 'Spirit Temple',
-            b: 'Fire Temple',
-            c: 'Shadow Temple',
-            d: 'Light Temple'
+            a: '1',
+            b: '3',
+            c: '5',
+            d: '7'
+        }
+    },
+    {
+        number: 12,
+        text: 'In GTA: London, which was a DLC pack for the original GTA game, the “WASTED” message that appears when you die is replaced by which message?',
+        options: {
+            a: 'You’re apples and pears!',
+            b: 'You’re brown bread!',
+            c: 'You’re nicked!',
+            d: 'You’re cozzers!'
+        }
+    },
+    {
+        number: 13,
+        text: 'Muammar al-Gaddafi was a Libyan politician who was assassinated in 2011, after 41 years of totalitarian rule. Watching a supposed mobile camera capture from the ruins of his ousted regime, gaming news sites spotted something strange on one of the walls – a giant reproduction of the cover art to a 2004 game. It seems Moatassem-Billah Gaddafi, son to an overthrown ruler, with his long, dark hair, had found some parallels with the main character depicted. Which video game series was the art work from?',
+        options: {
+            a: 'Prince of Persia',
+            b: 'Assassin’s Creed',
+            c: 'Castlevania',
+            d: 'Fire Emblem'
+        }
+    },
+    {
+        number: 14,
+        text: 'According to a report by Game, as of September 2020, Fortnite is the most-played game worldwide in terms of cumulative time spent in-game. With the knowledge that modern humans have been on the planet for 200,000 years, how many more times than that has Fortnite been played?',
+        options: {
+            a: '52 times longer than humans have been on the planet',
+            b: '2 times longer than humans have been on the planet',
+            c: '20 times longer than humans have been on the planet',
+            d: '6 times longer than humans been on the planet'
+        }
+    },
+    {
+        number: 15,
+        text: 'Sometimes developers have been known to include memorials in their games to people who have passed away. Which of these games does not have an in-game memorial added by developers?',
+        options: {
+            a: 'Star Trek Online',
+            b: 'Overwatch',
+            c: 'Middle-Earth: Shadow of War',
+            d: 'Amnesia: The Dark Descent'
         }
     }
 ];
 
 const answers = [
-    'a',
-    'b',
-    'b',
-    'b',
-    'c',
-    'a',
     'd',
-    'b',
+    'd',
+    'a',
     'c',
     'b',
+    'b',
+    'a',
+    'c',
+    'd',
+    'a',
+    'c',
+    'b',
+    'a',
+    'a',
     'd'
 ];
 
